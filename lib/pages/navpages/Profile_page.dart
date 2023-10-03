388391a131d979b534bfa84e2b7d189580c85d66
Jhonatan Durango <durango0315@gmail.com>
2023-10-03 15:56:09 -0500

    empezar la lista inicio de sesion

--- lib/pages/navpages/Profile_page.dart
+++ lib/pages/navpages/Profile_page.dart
@@ -14,7 +14,7 @@ class _ProfilePageState extends State<ProfilePage> {
   TextEditingController emailController = TextEditingController();
   TextEditingController passwordController = TextEditingController();
 
-  bool isLoggedIn = false;
+  bool isLoggedIn = false; // Variable para controlar si el usuario está conectado
 
   Future<void> login() async {
     final email = emailController.text;
@@ -85,186 +85,212 @@ class _ProfilePageState extends State<ProfilePage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      resizeToAvoidBottomInset: false,
-      body: Container(
-        decoration: BoxDecoration(
-          gradient: LinearGradient(
-            begin: Alignment.topRight,
-            end: Alignment.bottomLeft,
-            colors: [
-              Color.fromARGB(255, 2, 54, 4),
-              Color.fromRGBO(184, 212, 50, 1),
-            ],
-          ),
+    // Si el usuario está conectado, muestra un contenido diferente.
+    if (isLoggedIn) {
+      return Scaffold(
+        appBar: AppBar(
+          title: Text('Perfil de usuario'),
+          actions: [
+            IconButton(
+              icon: Icon(Icons.logout),
+              onPressed: () {
+                // Implementa la lógica para cerrar sesión aquí
+                setState(() {
+                  isLoggedIn = false;
+                });
+              },
+            ),
+          ],
         ),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            // Imagen
-            Image.asset(
-              'assets/logo_profile.png',
-              width: double.infinity,
-              height: 200,
+        body: Center(
+          child: Text('Contenido del perfil de usuario aquí'),
+        ),
+      );
+    } else {
+      // Si el usuario no está conectado, muestra el formulario de inicio de sesión.
+      return Scaffold(
+        resizeToAvoidBottomInset: false,
+        body: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topRight,
+              end: Alignment.bottomLeft,
+              colors: [
+                Color.fromARGB(255, 2, 54, 4),
+                Color.fromRGBO(184, 212, 50, 1),
+              ],
             ),
-            SizedBox(
-              height: 40,
-            ),
-            // Cuadro de fondo blanco
-            Container(
-              width: 316,
-              height: 500,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(26),
-                color: Colors.white,
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              // Imagen
+              Image.asset(
+                'assets/logo_profile.png',
+                width: double.infinity,
+                height: 200,
               ),
-              padding: EdgeInsets.all(16.0),
-              child: Column(
-                children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text(
-                        'Iniciar sesión',
-                        style: TextStyle(
-                          fontSize: 24.0,
-                          fontWeight: FontWeight.bold,
+              SizedBox(
+                height: 40,
+              ),
+              // Cuadro de fondo blanco
+              Container(
+                width: 316,
+                height: 500,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(26),
+                  color: Colors.white,
+                ),
+                padding: EdgeInsets.all(16.0),
+                child: Column(
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          'Iniciar sesión',
+                          style: TextStyle(
+                            fontSize: 24.0,
+                            fontWeight: FontWeight.bold,
+                          ),
                         ),
-                      ),
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text('No tienes cuenta?'),
-                          TextButton(
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text('No tienes cuenta?'),
+                            TextButton(
+                              onPressed: () {
+                                Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) => SignUpPageID()),
+                                );
+                              },
+                              style: ButtonStyle(backgroundColor: null),
+                              child: Text(
+                                'Regístrate',
+                                style: TextStyle(
+                                  color: Color.fromARGB(255, 131, 221, 96),
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                    SizedBox(
+                      height: 34,
+                    ),
+                    // Campos de entrada de texto para correo electrónico y contraseña
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Ingresa tu correo electrónico',
+                          style: TextStyle(
+                              fontSize: 16, fontWeight: FontWeight.w500),
+                        ),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        Container(
+                          height: 50,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(12),
+                            border: Border.all(
+                              color: Colors.blueAccent,
+                            ),
+                          ),
+                          child: TextField(
+                            controller: emailController,
+                            decoration: InputDecoration(
+                              hintText: '    Correo electrónico',
+                              border: InputBorder.none,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                    SizedBox(
+                      height: 34,
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Ingresa tu contraseña',
+                          style: TextStyle(
+                              fontSize: 16, fontWeight: FontWeight.w500),
+                        ),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        Container(
+                          height: 50,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(12),
+                            border: Border.all(
+                              color: Colors.blueAccent,
+                            ),
+                          ),
+                          child: TextField(
+                            controller: passwordController,
+                            decoration: InputDecoration(
+                              hintText: '    Contraseña',
+                              border: InputBorder.none,
+                            ),
+                            obscureText: true,
+                          ),
+                        ),
+                        Align(
+                          alignment: Alignment.centerRight,
+                          child: TextButton(
                             onPressed: () {
-                              Navigator.push(
-                                context,
+                              Navigator.of(context).push(
                                 MaterialPageRoute(
-                                    builder: (context) => SignUpPageID()),
+                                  builder: (context) {
+                                    return ForgotPasswordPage();
+                                  },
+                                ),
                               );
                             },
-                            style: ButtonStyle(backgroundColor: null),
                             child: Text(
-                              'Regístrate',
+                              'Olvidé mi contraseña',
                               style: TextStyle(
                                 color: Color.fromARGB(255, 131, 221, 96),
                               ),
                             ),
                           ),
-                        ],
-                      ),
-                    ],
-                  ),
-                  SizedBox(
-                    height: 34,
-                  ),
-                  // Campos de entrada de texto para correo electrónico y contraseña
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        'Ingresa tu correo electrónico',
-                        style: TextStyle(
-                            fontSize: 16, fontWeight: FontWeight.w500),
-                      ),
-                      SizedBox(
-                        height: 10,
-                      ),
-                      Container(
-                        height: 50,
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(12),
-                          border: Border.all(
-                            color: Colors.blueAccent,
-                          ),
                         ),
-                        child: TextField(
-                          controller: emailController,
-                          decoration: InputDecoration(
-                            hintText: '    Correo electrónico',
-                            border: InputBorder.none,
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                  SizedBox(
-                    height: 34,
-                  ),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        'Ingresa tu contraseña',
-                        style: TextStyle(
-                            fontSize: 16, fontWeight: FontWeight.w500),
-                      ),
-                      SizedBox(
-                        height: 10,
-                      ),
-                      Container(
-                        height: 50,
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(12),
-                          border: Border.all(
-                            color: Colors.blueAccent,
-                          ),
-                        ),
-                        child: TextField(
-                          controller: passwordController,
-                          decoration: InputDecoration(
-                            hintText: '    Contraseña',
-                            border: InputBorder.none,
-                          ),
-                          obscureText: true,
-                        ),
-                      ),
-                      Align(
-                        alignment: Alignment.centerRight,
-                        child: TextButton(
-                          onPressed: () {
-                            Navigator.of(context).push(
-                              MaterialPageRoute(
-                                builder: (context) {
-                                  return ForgotPasswordPage();
-                                },
-                              ),
-                            );
-                          },
-                          child: Text(
-                            'Olvidé mi contraseña',
-                            style: TextStyle(
-                              color: Color.fromARGB(255, 131, 221, 96),
-                            ),
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                  SizedBox(
-                    height: 34,
-                  ),
-                  ElevatedButton(
-                    onPressed: () {
-                      login();
-                    },
-                    style: ButtonStyle(
-                        minimumSize: MaterialStateProperty.all(Size(250, 48)),
-                        backgroundColor: MaterialStateProperty.all(
-                            Color.fromARGB(255, 2, 82, 4)),
-                        shape: MaterialStateProperty.all(RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(12)))),
-                    child: Text(
-                      'Iniciar sesión',
-                      style: TextStyle(color: Colors.white),
+                      ],
                     ),
-                  ),
-                ],
+                    SizedBox(
+                      height: 34,
+                    ),
+                    ElevatedButton(
+                      onPressed: () {
+                        login();
+                      },
+                      style: ButtonStyle(
+                          minimumSize:
+                              MaterialStateProperty.all(Size(250, 48)),
+                          backgroundColor: MaterialStateProperty.all(
+                              Color.fromARGB(255, 2, 82, 4)),
+                          shape: MaterialStateProperty.all(
+                              RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(12)))),
+                      child: Text(
+                        'Iniciar sesión',
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    ),
+                  ],
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
-      ),
-    );
+      );
+    }
   }
 }
